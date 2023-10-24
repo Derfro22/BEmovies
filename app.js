7889294bad08aede613eca7035c019e7887774a3
Fabrice Wasterlain <f.wasterlain@gmail.com>
2023-10-24 10:26:05 +0200

    first test

---
+++ app.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 19,
+    navigation: {
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
+        clickable: true,
+      },
+  });
+
+  var newSwiper = new Swiper(".mySwiper1", {
+    slidesPerView: 4,
+    spaceBetween: 19,
+    navigation: {
+        nextEl: ".button-next1",
+        prevEl: ".button-prev1",
+        clickable: true,
+      },
+  });
