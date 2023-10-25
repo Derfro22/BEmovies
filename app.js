837e0974fb93297d36c7609003ace4755eabc335
Fabrice Wasterlain <f.wasterlain@gmail.com>
2023-10-25 16:27:27 +0200

    Html and CSS ok !

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-var swiper = new Swiper(".mySwiper", {
-  direction: 'horizontal',
+const swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
   spaceBetween: 19,
-  centerInsufficientSlides: true,
   navigation: {
-    nextEl: ".button-next",
-    prevEl: ".button-prev",
-  }
+      nextEl: ".button-next",
+      prevEl: ".button-prev",
+      clickable: true,
+    },
+});
+
+const swiper1 = new Swiper(".mySwiper1", {
+  slidesPerView: 4,
+  spaceBetween: 19,
+  navigation: {
+      nextEl: ".button-next1",
+      prevEl: ".button-prev1",
+      clickable: true,
+    },
+});
+
+const swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 4,
+  spaceBetween: 19,
+  navigation: {
+      nextEl: ".button-next2",
+      prevEl: ".button-prev2",
+      clickable: true,
+    },
 });
